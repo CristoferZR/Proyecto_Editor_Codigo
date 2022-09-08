c8ad720b4e0da493b88ac7dce85c199fd2b08cd5
CristoferZR <zamoracristofer387@gmail.com>
2022-09-08 01:19:52 -0500

    Se agrega el proyecto practica editor

---
+++ js/app.dev.js
@@ -0,0 +1,12 @@
+
+
+
+const update=()=>{
+    const text=document.querySelector("#text")
+    const view=document.querySelector("#view")
+
+    view.srcdoc=text.value;
+
+    //console.log(text.value);
+    
+}
